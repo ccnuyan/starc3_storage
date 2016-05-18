7833fc4db86142cc5e49c58eb9223e86a963a1c2
ccnuyan <ccnuyan@live.com>
2016-05-18 09:13:55 +0800

    Revert "multifile upload"
    This reverts commit 5490602da502f0f8839918b9bab2baf3a492c0e2.

--- src/transaction.model.js
+++ src/transaction.model.js
@@ -2,73 +2,65 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UploadTransaction = new Schema({
-    requestMethod: {
-        type: String,
-        required: true,
-    },
-    storage_box_id: {
-        type: String,
-        required: true,
-    },
-    requestUri: {
-        type: String,
-        required: true,
-    },
-    requestBody: {
-        type: String
-    },
-    authorization: {
-        type: String
-    },
-    created: {
-        type: Date,
-        default: Date.now
-    },
-    total: {
-        type: Number,
-        default: 1
-    },
-    done: {
-        type: Number,
-        default: 1
-    }
+  requestMethod: {
+    type: String,
+    required: true,
+  },
+  storage_box_id: {
+    type: String,
+    required: true,
+  },
+  requestUri: {
+    type: String,
+    required: true,
+  },
+  requestBody: {
+    type: String
+  },
+  authorization: {
+    type: String
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 var DownloadTransaction = new Schema({
-    storage_object_id: {
-        type: String,
-        required: true,
-    },
-    storage_box_id: {
-        type: String,
-        required: true,
-    },
-    fileName: {
-        type: String
-    },
-    created: {
-        type: Date,
-        default: Date.now
-    }
+  storage_object_id: {
+    type: String,
+    required: true,
+  },
+  storage_box_id: {
+    type: String,
+    required: true,
+  },
+  fileName: {
+    type: String
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 var CopyTransaction = new Schema({
-    storage_object_id: {
-        type: String,
-        required: true,
-    },
-    storage_box_id: {
-        type: String,
-        required: true,
-    },
-    created: {
-        type: Date,
-        default: Date.now
-    }
+  storage_object_id: {
+    type: String,
+    required: true,
+  },
+  storage_box_id: {
+    type: String,
+    required: true,
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = function() {
-    mongoose.model('UploadTransaction', UploadTransaction);
-    mongoose.model('DownloadTransaction', DownloadTransaction);
-    mongoose.model('CopyTransaction', CopyTransaction);
+  mongoose.model('UploadTransaction', UploadTransaction);
+  mongoose.model('DownloadTransaction', DownloadTransaction);
+  mongoose.model('CopyTransaction', CopyTransaction);
 };
