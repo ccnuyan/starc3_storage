8bed5b959940edb4cf6fd9be0468a701f7774652
yan zhonghua <ccnuyan@yans-MacBook-Pro.local>
2016-10-17 21:18:03 +0800

    fixed

--- src/services/swift.js
+++ src/services/swift.js
@@ -29,7 +29,7 @@ function fileName(headerValue) {
 
     var filename = m[1].substr(m[1].lastIndexOf('\\') + 1);
     filename = filename.replace(/%22/g, '"');
-    filename = filename.replace(/&#([\d]{4});/g, function(m, code) {
+    filename = filename.replace(/&#([\d]{4});/g, function (m, code) {
         return String.fromCharCode(code);
     });
 
@@ -44,7 +44,7 @@ function multiPart(options) {
 
     parser.initWithBoundary(options.boundary);
 
-    parser.onPartBegin = function() {
+    parser.onPartBegin = function () {
         part.headers = {};
         part.name = null;
         part.filename = null;
@@ -53,15 +53,15 @@ function multiPart(options) {
         headerValue = '';
     };
 
-    parser.onHeaderField = function(b, start, end) {
+    parser.onHeaderField = function (b, start, end) {
         headerField += b.toString(options.encoding, start, end);
     };
 
-    parser.onHeaderValue = function(b, start, end) {
+    parser.onHeaderValue = function (b, start, end) {
         headerValue += b.toString(options.encoding, start, end);
     };
 
-    parser.onHeaderEnd = function() {
+    parser.onHeaderEnd = function () {
         headerField = headerField.toLowerCase();
         part.headers[headerField] = headerValue;
 
@@ -82,13 +82,13 @@ function multiPart(options) {
         headerValue = '';
     };
 
-    parser.onHeadersEnd = function() {
+    parser.onHeadersEnd = function () {
         if (options.onHeadersEnd) {
             options.onHeadersEnd(part);
         }
     };
 
-    parser.onPartData = function(b, start, end) {
+    parser.onPartData = function (b, start, end) {
         if (options.onPartData) {
             options.onPartData(b.slice(start, end));
         }
@@ -116,7 +116,7 @@ function Swift(options, callback) {
             'X-Storage-User': this.options.user,
             'X-Storage-Pass': this.options.pass
         }
-    }, function(err, res) {
+    }, function (err, res) {
         if (!err && res.headers['x-storage-url'] && res.headers['x-auth-token']) {
             self.account = res.headers['x-storage-url'].split('v1/')[1];
             self.token = res.headers['x-auth-token'];
@@ -132,7 +132,7 @@ function Swift(options, callback) {
  */
 
 
-Swift.prototype.request = function(options, callback, pipe) {
+Swift.prototype.request = function (options, callback, pipe) {
 
     var downloadFlag = pipe && pipe.res;
     var uploadFlag = pipe && pipe.req;
@@ -154,18 +154,18 @@ Swift.prototype.request = function(options, callback, pipe) {
     options.path = encodeURI(options.path);
 
     if (otherBusinessFlag) {
-        var businessReq = protocol.request(options, function(res) {
+        var businessReq = protocol.request(options, function (res) {
             var buffers = [];
             if (downloadFlag) {
                 pipe.res.header('Content-Length', res.headers['content-length']);
                 pipe.res.header('Content-Type', res.headers['content-type']);
             }
 
-            res.on('data', function(buffer) {
+            res.on('data', function (buffer) {
                 buffers.push(buffer);
             });
 
-            res.on('end', function(err) {
+            res.on('end', function (err) {
                 res.body = buffers.join('');
                 if (callback) {
                     callback(err, res);
@@ -173,10 +173,8 @@ Swift.prototype.request = function(options, callback, pipe) {
             });
         });
 
-        businessReq.on('error', function(err) {
-            if (callback) {
-                callback(err);
-            }
+        businessReq.on('error', function (err) {
+            console.log('businessReq error:' + err);
         });
 
         return businessReq.end();
@@ -184,11 +182,11 @@ Swift.prototype.request = function(options, callback, pipe) {
 
     if (downloadFlag) {
 
-        var downloadReq = protocol.request(options, function(res) {
+        var downloadReq = protocol.request(options, function (res) {
 
             pipe.res.header('Content-Length', res.headers['content-length']);
 
-            res.on('data', function(buffer) {
+            res.on('data', function (buffer) {
                 if (res.statusCode >= 400) {
                     if (callback) {
                         callback({
@@ -201,15 +199,13 @@ Swift.prototype.request = function(options, callback, pipe) {
                 }
             });
 
-            res.on('end', function(err) {
+            res.on('end', function (err) {
                 callback(err);
             });
         });
 
-        downloadReq.on('error', function(err) {
-            if (callback) {
-                callback(err);
-            }
+        downloadReq.on('error', function (err) {
+            console.log('downloadReq error:' + err);
         });
 
         return downloadReq.end();
@@ -222,42 +218,42 @@ Swift.prototype.request = function(options, callback, pipe) {
         var bytesReceived = 0;
 
         var parser = options.boundary ? multiPart(extend(options, {
-            onHeadersEnd: function() {
-                uploadReq = protocol.request(options, function(res) {
+            onHeadersEnd: function () {
+                uploadReq = protocol.request(options, function (res) {
 
-                    res.on('data', function() {
-                        if (res.statusCode >= 400) {
-                            if (callback) {
-                                callback({
-                                    statusCode: res.statusCode,
-                                    body: res.body
-                                });
-                            }
-                        } else {
-                            callback();
-                        }
-                    });
+                    // res.on('data', function () {
+                    //     if (res.statusCode >= 400) {
+                    //         if (callback) {
+                    //             callback({
+                    //                 statusCode: res.statusCode,
+                    //                 body: res.body
+                    //             });
+                    //         }
+                    //     } else {
+                    //         callback();
+                    //     }
+                    // });
 
-                    res.on('end', function(err) {
+                    res.on('end', function (err) {
                         callback(err, res);
                     });
                 });
 
-                uploadReq.on('error', function(err) {
-                    callback(err);
+                uploadReq.on('error', function (err) {
+                    console.log('downloadReq error:' + err);
                 });
             },
-            onPartData: function(buffer) {
+            onPartData: function (buffer) {
                 uploadReq.write(buffer);
             }
         })) : null;
 
-        pipe.req.on('data', function(buffer) {
+        pipe.req.on('data', function (buffer) {
             parser.write(buffer);
             pipe.req.emit('progress', bytesReceived += buffer.length, options.contentLength || options.headers['Content-Length']);
         });
 
-        pipe.req.on('end', function() {
+        pipe.req.on('end', function () {
             if (uploadReq) {
                 uploadReq.end();
             } else {
@@ -269,14 +265,14 @@ Swift.prototype.request = function(options, callback, pipe) {
 };
 
 // List Containers
-Swift.prototype.listContainers = function(callback) {
+Swift.prototype.listContainers = function (callback) {
     this.request({
         path: '/v1.0/' + this.account + '?format=json'
     }, callback);
 };
 
 // Retrieve Account Metadata *
-Swift.prototype.retrieveAccountMetadata = function(callback) {
+Swift.prototype.retrieveAccountMetadata = function (callback) {
     this.request({
         path: '/v1.0/' + this.account,
         method: 'HEAD'
@@ -289,14 +285,14 @@ Swift.prototype.retrieveAccountMetadata = function(callback) {
  */
 
 // List Objects
-Swift.prototype.listObjects = function(object, callback) {
+Swift.prototype.listObjects = function (object, callback) {
     this.request({
         path: '/v1.0/' + this.account + '/' + object + '?format=json'
     }, callback);
 };
 
 // Create Container
-Swift.prototype.createContainer = function(container, callback) {
+Swift.prototype.createContainer = function (container, callback) {
     this.request({
         path: '/v1.0/' + this.account + '/' + container,
         method: 'PUT'
@@ -304,22 +300,22 @@ Swift.prototype.createContainer = function(container, callback) {
 };
 
 // Delete Container
-Swift.prototype.clearContainer = function(container, callback) {
+Swift.prototype.clearContainer = function (container, callback) {
     var self = this;
     var objects = [];
 
-    this.listObjects(container, function(err, result) {
+    this.listObjects(container, function (err, result) {
         try {
             objects = JSON.parse(result.body);
-        } catch (e) {}
-        objects.forEach(function(obj) {
+        } catch (e) { }
+        objects.forEach(function (obj) {
             self.deleteObject(container, obj.name);
         });
     });
 };
 
 // Retrieve Container Metadata *
-Swift.prototype.retrieveContainerMetadata = function(container, callback) {
+Swift.prototype.retrieveContainerMetadata = function (container, callback) {
     this.request({
         path: '/v1.0/' + this.account + '/' + container,
         method: 'HEAD'
@@ -332,24 +328,24 @@ Swift.prototype.retrieveContainerMetadata = function(container, callback) {
  */
 
 // Object stream on pipe
-Swift.prototype.getFile = function(container, object, callback, res) {
+Swift.prototype.getFile = function (container, object, callback, res) {
     this.request({
         path: '/v1.0/' + this.account + '/' + container + '/' + object
     }, callback, {
-        res: res
-    });
+            res: res
+        });
 };
 
 // Create/Update Object
-Swift.prototype.createObject = Swift.prototype.updateObject = function(container, object, callback, req) {
+Swift.prototype.createObject = Swift.prototype.updateObject = function (container, object, callback, req) {
     var options = {
         path: '/v1.0/' + this.account + '/' + container + '/' + object,
         method: 'PUT',
         filename: object,
         headers: {
             'X-Auth-Token': this.token
-                //, 'ETag': crypto.createHash('md5').update(container + '/' + object).digest('hex')
-                //, 'X-Object-Meta-PIN': 1234
+            //, 'ETag': crypto.createHash('md5').update(container + '/' + object).digest('hex')
+            //, 'X-Object-Meta-PIN': 1234
         }
     };
 
@@ -378,14 +374,14 @@ Swift.prototype.createObject = Swift.prototype.updateObject = function(container
 };
 
 // Retrieve Object *
-Swift.prototype.retrieveObject = function(container, object, callback) {
+Swift.prototype.retrieveObject = function (container, object, callback) {
     this.request({
         path: '/v1.0/' + this.account + '/' + container + '/' + object
     }, callback);
 };
 
 // Delete Object
-Swift.prototype.deleteObject = function(container, object, callback) {
+Swift.prototype.deleteObject = function (container, object, callback) {
     this.request({
         path: '/v1.0/' + this.account + '/' + container + '/' + object,
         method: 'DELETE'
@@ -393,7 +389,7 @@ Swift.prototype.deleteObject = function(container, object, callback) {
 };
 
 // Retrieve Object Metadata *
-Swift.prototype.retrieveObjectMetadata = function(container, object, callback) {
+Swift.prototype.retrieveObjectMetadata = function (container, object, callback) {
     this.request({
         path: '/v1.0/' + this.account + '/' + container + '/' + object,
         method: 'HEAD'
@@ -401,7 +397,7 @@ Swift.prototype.retrieveObjectMetadata = function(container, object, callback) {
 };
 
 // Update Object Metadata *
-Swift.prototype.updateObjectMetadata = function(container, object, callback) {
+Swift.prototype.updateObjectMetadata = function (container, object, callback) {
     this.request({
         path: '/v1.0/' + this.account + '/' + container + '/' + object,
         method: 'POST'
@@ -409,7 +405,7 @@ Swift.prototype.updateObjectMetadata = function(container, object, callback) {
 };
 
 // Copy Object
-Swift.prototype.copyObject = function(container, destObject, fromContainer, sourceObject, callback) {
+Swift.prototype.copyObject = function (container, destObject, fromContainer, sourceObject, callback) {
     this.request({
         path: '/v1.0/' + this.account + '/' + container + '/' + destObject,
         method: 'PUT',
